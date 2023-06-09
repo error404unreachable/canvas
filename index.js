b7f1dcbda46e374ef13812998b7bf68fa2d93330
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-06-09 15:31:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.getElementById("myCanvas");
+const c = canvas.getContext("2d");
+
+c.fillStyle = "red";
+c.fillRect(20, 20, 0, 0);
