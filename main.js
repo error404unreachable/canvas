57c742dcd522f9a69b59a82ee2b983e8d55a7c75
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-02-27 15:03:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let canvas = document.getElementById("canvas");
-
+let count = 0;
 let context = canvas.getContext("2d");
 
+for (count == 4) {
 var img = new Image(),
     x = canvas.width,
-    y = 0;
+    y = 0,
+    count += 1;
+}
 
 img.onload = randomizer;
 img.src = "Red.png";   // load image
