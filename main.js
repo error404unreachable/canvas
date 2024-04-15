0b07af09e69cc841a3653605504a72cd24553205
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-04-15 14:49:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,8 @@ key.src = "Red.png";   // load image
 
 function animate() {
   context.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
- /* context.save();
-  context.rotate(180*Math.PI/180);*/
-  context.drawImage(key, x, y);               
-  //context.restore();
+  context.rotate(180);
+  context.drawImage(key, x, y);
   x -= 4;
   if (x > 250) requestAnimationFrame(animate)  
     // draw image at current position
